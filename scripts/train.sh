bd125431bd307879295c52848356667f1c861bd2
Arseny Tolmachev <eiennohito@gmail.com>
2018-06-11 20:27:40 +0900

    pass epochs and num of iters via env variable

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,7 +6,7 @@ CORPUS="$3"
 OUTPUT_MODEL="$4"
 
 NTHREADS="${NTHREADS:-4}"
-EPOCHS="${EPOCHS:-5}"
+EPOCHS="${EPOCHS:-10}"
 ITERS="${ITERS:-5}"
 MODEL_SIZE="${MODEL_SIZE:-22}"
 LOC_BEAM="${LOC_BEAM:-4}"
@@ -21,7 +21,7 @@ set -x
     --size="$MODEL_SIZE" \
     --beam="$LOC_BEAM" \
     --threads="${NTHREADS}" \
-    --max-batch-iters=5 --max-epochs=10 --epsilon=1e-7 \
+    --max-batch-iters="$ITERS" --max-epochs="$EPOCHS" --epsilon=1e-7 \
     --batch="100000" \
     --gb-left-min=4 --gb-left-max=4 \
     --gb-right-min=4 --gb-right-max=4 \
